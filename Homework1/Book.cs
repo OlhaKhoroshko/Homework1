2a6f036eddf9a32c0a402ed7145e90c28f0d3467
Olha <khoroshkoolga.buy@gmail.com>
2022-07-01 22:02:02 +0300

    Homework 1 20220701

---
+++ Homework1/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Autor { get; set; }
+    }
+}
---
+++ Homework1/Controllers/BookController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Homework1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> Books { get; set; }
+        static BookController()
+        {
+            Books = new List<Book>
+                    {
+                        new Book
+                        {
+                            ID = 1,
+                            Name = "C# для профессионалов. Тонкости программирования.",
+                            Autor = "Джон Скит"
+                        }
+                    };
+        }
+        private readonly ILogger<BookController> _logger;
+
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public void Create(Book book)
+        {
+            book.ID = Books.Select(x => x.ID).Max() + 1;
+            Books.Add(book);
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, Book book)
+        {
+            Books[id] = book;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Books.Remove(Books[id]);
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return Books;
+        }
+
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            return Books.FirstOrDefault(x => x.ID == id);
+        }
+    }
+}
